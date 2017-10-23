60a1452afa288ae7699309be32e3ba5e9efe20d5
bbname <bartlomiej.bienczyk@gmail.com>
2017-10-23 17:34:25 +0200

    I've added new controller for image actions.

---
+++ Zin-Service.WebUI/Controllers/ImageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Zin_Service.WebUI.Controllers
+{
+    public class ImageController : Controller
+    {
+
+        [HttpPost]
+        public void UploadImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateFilterImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public FileResult DownloadImage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
